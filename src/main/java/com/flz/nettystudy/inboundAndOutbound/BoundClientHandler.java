9a1e520dbc42bcc32391f32bf3f07147273849fa
Fallenz <beijuspsn@163.com>
2023-07-11 18:39:22 +0800

    add log and print content

--- src/main/java/com/flz/nettystudy/inboundAndOutbound/BoundClientHandler.java
+++ src/main/java/com/flz/nettystudy/inboundAndOutbound/BoundClientHandler.java
@@ -6,7 +6,7 @@ import io.netty.channel.SimpleChannelInboundHandler;
 public class BoundClientHandler extends SimpleChannelInboundHandler<Long> {
     @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, Long aLong) throws Exception {
-        System.out.println("[BoundClientHandler] received from server");
+        System.out.println("[BoundClientHandler] received from server:" + aLong);
         // 给客户端响应数据
         channelHandlerContext.channel().writeAndFlush(aLong * 2);
     }
--- src/main/java/com/flz/nettystudy/inboundAndOutbound/LongTypeDecoder.java
+++ src/main/java/com/flz/nettystudy/inboundAndOutbound/LongTypeDecoder.java
@@ -9,6 +9,7 @@ import java.util.List;
 public class LongTypeDecoder extends ByteToMessageDecoder {
     @Override
     protected void decode(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf, List<Object> list) throws Exception {
+        System.out.println("[LongTypeDecoder] executed");
         // 按8字节拆分读取的数据，如果一次性没读完，则会循环直到读完，循环会导致后续的inbound handler被调用多次，导致解码多次
         if (byteBuf.readableBytes() >= 8) {
             // list的数据会让下一个inbound handler接收
