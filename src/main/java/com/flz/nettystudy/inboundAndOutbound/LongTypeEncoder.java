54c718a95fb7cd59ebb736e2af32c1d0b9d81d9a
Fallenz <beijuspsn@163.com>
2023-07-11 18:39:04 +0800

    write directly

--- src/main/java/com/flz/nettystudy/inboundAndOutbound/LongTypeEncoder.java
+++ src/main/java/com/flz/nettystudy/inboundAndOutbound/LongTypeEncoder.java
@@ -7,9 +7,7 @@ import io.netty.handler.codec.MessageToByteEncoder;
 public class LongTypeEncoder extends MessageToByteEncoder<Long> {
     @Override
     protected void encode(ChannelHandlerContext channelHandlerContext, Long aLong, ByteBuf byteBuf) throws Exception {
-        // 按8字节拆分读取的数据，如果一次性没读完，则会循环直到读完，循环会导致后续的outbound handler被调用多次，导致编码多次
-        if (byteBuf.readableBytes() >= 8) {
-            byteBuf.readLong();
-        }
+        System.out.println("[LongTypeEncoder] executed");
+        byteBuf.writeLong(aLong);
     }
 }
