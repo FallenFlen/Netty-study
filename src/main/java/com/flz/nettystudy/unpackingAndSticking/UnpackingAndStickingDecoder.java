dd0818119b59c035170de397235e17b14321fcfa
Fallenz <beijuspsn@163.com>
2023-07-13 14:58:18 +0800

    add comment

--- src/main/java/com/flz/nettystudy/unpackingAndSticking/UnpackingAndStickingDecoder.java
+++ src/main/java/com/flz/nettystudy/unpackingAndSticking/UnpackingAndStickingDecoder.java
@@ -12,6 +12,7 @@ public class UnpackingAndStickingDecoder extends ReplayingDecoder<UnpackingAndSt
     protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
         System.out.println("[UnpackingAndStickingDecoder] 启动!");
         int length = in.readInt();
+        // 这里的buffer就被限制了长度，长度取决于发送方消息的长度，因此读取到的数据也被限制了长度，固能读出单次发送的完整消息
         byte[] buffer = new byte[length];
         in.readBytes(buffer);
         UnpackingAndStickingMessageProtocol messageProtocol = UnpackingAndStickingMessageProtocol.of(length, buffer);
