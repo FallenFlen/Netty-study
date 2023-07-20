6a9c9a3b3abb0c8c192f3d01907153add2f36207
Fallenz <beijuspsn@163.com>
2023-07-20 16:53:37 +0800

    add blocked client abstract class

---
+++ src/main/java/com/flz/nettystudy/common/base/AbstractCommonCustomContinuousClient.java
@@ -0,0 +1,39 @@
+package com.flz.nettystudy.common.base;
+
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * 通用持续性客户端，发完消息不会断开连接
+ */
+public abstract class AbstractCommonCustomContinuousClient extends BaseClient implements NamedEndpoint {
+    private ChannelInitializer<SocketChannel> channelInitializer;
+
+    public AbstractCommonCustomContinuousClient(String host, int port, ChannelInitializer<SocketChannel> channelInitializer) {
+        super(host, port);
+        this.channelInitializer = channelInitializer;
+    }
+
+    @Override
+    protected void doConnect() throws Throwable {
+        ChannelFuture channelFuture = this.bootstrap.group(this.group)
+                .channel(NioSocketChannel.class)
+                .handler(this.channelInitializer)
+                .connect(this.host, this.port)
+                .sync()
+                .addListener(future -> {
+                    if (future.isSuccess()) {
+                        System.out.printf("[%s] Started!\n", getEndpointDescription());
+                    }
+                });
+
+        channelFuture.channel().closeFuture().sync();
+    }
+
+    @Override
+    public String getEndpointDescription() {
+        return getClass().getName();
+    }
+}
